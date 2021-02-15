5632ea105fe7b0b8bf79ceb6d7441926a68b4ccf
Yash Prakash Mishra <34412462+xcalibre1@users.noreply.github.com>
2021-02-15 14:11:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,15 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
 
-class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
+function App() {
+  const now = new Date().toLocaleTimeString();
+  const [Time, setTime] = useState(now);
+  useEffect(() => {
+    let id = setInterval(() => {
+      let newTime = new Date().toLocaleTimeString();
+      setTime(newTime);
+    });
+    return () => clearInterval(id);
+  }, [Time]);
+  return <div className="Clock"><h3>{Time}</h3></div>;
 }
-
 
 export default App;
